25b3838b50bc6f4321acb32f9ef059f1347368cb
Fourier2718281828 <non-linear@ukr.net>
2023-11-11 13:38:47 +0200

    added: BaseUpgradeScriptableObject

---
+++ FleshAndBones/Assets/Scripts/Upgrades/BaseUpgradeScriptableObject.cs
@@ -0,0 +1,29 @@
+﻿using App.World.Entity.Player.PlayerComponents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace App.Upgrades
+{
+    public abstract class BaseUpgradeScriptableObject<UpgradableEntity> 
+        : ScriptableObject
+        , IDisplayableUpgrade
+        , IUpgradeAbstractVisitor
+        , IUpgradeVisitor<UpgradableEntity>
+
+        where UpgradableEntity : IUpgradable
+    {
+        public abstract string Description { get; }
+
+        public abstract bool IsComplete { get; }
+
+        public abstract void Disable();
+
+        public abstract void Enable(UpgradableEntity upgradable);
+
+        public abstract void LevelUp();
+    }
+}
