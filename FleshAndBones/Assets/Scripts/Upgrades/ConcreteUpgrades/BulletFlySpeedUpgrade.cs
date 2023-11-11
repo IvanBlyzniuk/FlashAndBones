d69225d24e22d7864622c285612c7bdbd6d3131f
Fourier2718281828 <non-linear@ukr.net>
2023-11-11 13:23:11 +0200

    added: BulletFlySpeedUpgrade

---
+++ FleshAndBones/Assets/Scripts/Upgrades/ConcreteUpgrades/BulletFlySpeedUpgrade.cs
@@ -0,0 +1,62 @@
+﻿using App.World.Entity.Player.PlayerComponents;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace App.Upgrades.ConcreteUpgrades
+{
+    [CreateAssetMenu(fileName = "BulletSpeedUp", menuName = "Scriptable Objects/Upgrades/BulletFlySpeedUpgrade")]
+    public class BulletFlySpeedUpgrade : ScriptableObject, IDisplayableUpgrade, IUpgradeAbstractVisitor, IUpgradeVisitor<Player>
+    {
+        [System.Serializable] struct UpgradeLevel
+        {
+            [Range(1f, 10f)] public float bulletFlySpeedMultiplier;
+        }
+
+        #region Serialized Fields
+        [SerializeField] private string description;
+        [SerializeField] private List<UpgradeLevel> levels;
+        #endregion
+
+        #region Fields
+        private Player upgradableEntity;
+        private UpgradeLevelManager<UpgradeLevel> levelManager;
+        private float initialBulletFlySpeed;
+        #endregion
+
+        #region Properties
+        public string Description => description;
+        public bool IsComplete => levelManager.IsComplete;
+        #endregion
+
+        #region Upgrading Itself
+        public void Enable(Player upgradable)
+        {
+            upgradableEntity = upgradable;
+            initialBulletFlySpeed = upgradableEntity.Weapon.BulletFlySpeed;
+        }
+
+        public void Disable()
+        {
+            upgradableEntity = null;
+            upgradableEntity.Weapon.BulletFlySpeed = initialBulletFlySpeed;
+            initialBulletFlySpeed = 0.0f;
+        }
+
+        public void LevelUp()
+        {
+            levelManager.LevelUp();
+            UpgradeLevel nextLevel = levelManager.CurrentLevel;
+            upgradableEntity.Weapon.BulletFlySpeed *= nextLevel.bulletFlySpeedMultiplier;
+        }
+        #endregion
+
+        public BulletFlySpeedUpgrade()
+        {
+            levels = new();
+            upgradableEntity = null;
+            initialBulletFlySpeed = 0.0f;
+            levelManager = new(levels);
+        }
+    }
+}
