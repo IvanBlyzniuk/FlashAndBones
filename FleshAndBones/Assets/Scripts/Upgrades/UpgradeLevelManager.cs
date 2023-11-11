e282cb199e9006c61c049d738ae32f8b9b3d25e4
Fourier2718281828 <non-linear@ukr.net>
2023-11-11 13:22:22 +0200

    added: UpgradeLevelManager

---
+++ FleshAndBones/Assets/Scripts/Upgrades/UpgradeLevelManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace App.Upgrades
+{
+    public class UpgradeLevelManager<LevelType>
+    {
+        private readonly List<LevelType> levels;
+        private int currentLevelIndex;
+
+        public bool IsComplete { get => currentLevelIndex == levels.Count - 1; }
+        public LevelType CurrentLevel => levels[currentLevelIndex];
+
+        public UpgradeLevelManager(List<LevelType> levels, int initialLevelIndex = 0)
+        {
+            if (levels.Count == 0)
+            {
+                throw new ArgumentException("Cannot manage an empty level set.");
+            }
+
+            if (currentLevelIndex < 0 || currentLevelIndex >= levels.Count) 
+            {
+                throw new IndexOutOfRangeException($"Input level index is out of level-set's bounds: {initialLevelIndex}");
+            }
+
+            this.levels = levels;
+            this.currentLevelIndex = initialLevelIndex;
+        }
+
+        public void LevelUp()
+        {
+            if (IsComplete)
+            {
+                throw new InvalidOperationException("Cannot level up an upgrade in a complete state.");
+            }
+
+            ++currentLevelIndex;
+        }
+    }
+}
